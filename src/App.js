ffc7e5f8f1212a4a487e19a2bcd38c7ee69f8cf3
Eduardo Fontenele <eduardo.fontenaille@gmail.com>
2022-06-26 23:33:46 -0300

    ==Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GlobalStyle from './styles/GlobalStyle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Routes from './routes';
+import { ToastContainer, toast } from 'react-toastify';
+
+import Nav from './components/Nav';
+
+export default function App() {
+	return (
+		<>
+			<Nav/>
+			<GlobalStyle/>
+			<Routes />
+			<ToastContainer
+				autoClose={1000}
+				position="top-right"
+				className="toast-container"/>
+		</>
+		
+	);
 }
-
-export default App;
